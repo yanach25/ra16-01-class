bd8a8147ca2cd44aa978938b0577ca2b37a3678a
Yana Chumburidze <ya.chum@gmail.com>
2022-02-19 13:53:49 +0300

    first commit

---
+++ src/shopItemFunc.js
@@ -0,0 +1,36 @@
+import {Item} from "./item";
+import PropTypes from "prop-types";
+import * as React from "react";
+
+class ShopItemFunc extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {item} = this.props;
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(Item)
+}
+
+export default ShopItemFunc;
